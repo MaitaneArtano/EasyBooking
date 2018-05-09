ec65b2f477772a756bf0ae2c04369a6ea25531f3
Maitane <maitaneartano@opendeusto.es>
2018-05-09 11:18:35 +0200

    avance

--- EasyBookingServer/src/Utils/PaymentAssembler.java
+++ EasyBookingServer/src/Utils/PaymentAssembler.java
@@ -1,5 +1,19 @@
 package Utils;
 
-public class PaymentAssembler {
+public class PaymentAssembler 
+{
+	
+	private static PaymentAssembler instance;
+
+	private PaymentAssembler() { }
+	
+	public static PaymentAssembler getInstance() 
+	{
+		if (instance == null) {
+			instance = new PaymentAssembler();
+		}
+		return instance;
+	}
+	
 
 }
