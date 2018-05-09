231e611894023f1da66f9561175a45423c82acbd
Maitane <maitaneartano@opendeusto.es>
2018-05-09 11:18:24 +0200

    avance

--- EasyBookingServer/src/Utils/AuthorisationAssembler.java
+++ EasyBookingServer/src/Utils/AuthorisationAssembler.java
@@ -1,5 +1,22 @@
 package Utils;
 
-public class AuthorisationAssembler {
+
+
+public class AuthorisationAssembler 
+{
+	
+	private static AuthorisationAssembler instance;
+
+	private AuthorisationAssembler() { }
+	
+	public static AuthorisationAssembler getInstance() 
+	{
+		if (instance == null) {
+			instance = new AuthorisationAssembler();
+		}
+		return instance;
+	}
+	
+	
 
 }
--- EasyBookingServer/src/Utils/VueloAssembler.java
+++ EasyBookingServer/src/Utils/VueloAssembler.java
@@ -1,5 +1,18 @@
 package Utils;
 
-public class VueloAssembler {
+public class VueloAssembler
+{
+	private static VueloAssembler instance;
+
+	private VueloAssembler() { }
+	
+	public static VueloAssembler getInstance() 
+	{
+		if (instance == null) {
+			instance = new VueloAssembler();
+		}
+		return instance;
+	}
+	
 
 }
