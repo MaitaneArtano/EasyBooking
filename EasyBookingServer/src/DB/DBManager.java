56f410df3304097bf6364aa998c7e4e7a1f640fc
Ianire <ianire.andola@opendeusto.es>
2018-05-21 12:02:39 +0200

    DBManager...

--- EasyBookingServer/src/DB/DBManager.java
+++ EasyBookingServer/src/DB/DBManager.java
@@ -24,27 +24,23 @@ public class DBManager
 		this.UsuarioCache = new ArrayList<>();
 		
 		Usuario user1 = new Usuario();
-		user1.setEmail("sample@gmail.com");
-		user1.setPassword("12345");		
+		user1.setEmail("sample@gmail.com");	
 		
 		Usuario user2 = new Usuario();
 		user2.setEmail("troyaikman08@hotmail.com");
-		user2.setPassword("12345");
 	
 		this.UsuarioCache.add(user1);
 		this.UsuarioCache.add(user2);
 		
 		Vuelo vuelo1 = new Vuelo();
-		vuelo1.setID_vuelo("A1");
+		vuelo1.setId_vuelo("A1");
 		vuelo1.setFecha("03/04/2018");
-		vuelo1.setCant_ticket(2);
 		vuelo1.setOrigen("Santander");
 		vuelo1.setDestino("Bali");
 		
 		Vuelo vuelo2 = new Vuelo();
-		vuelo2.setID_vuelo("A1");
+		vuelo2.setId_vuelo("A1");
 		vuelo2.setFecha("03/04/2018");
-		vuelo2.setCant_ticket(2);
 		vuelo2.setOrigen("Santander");
 		vuelo2.setDestino("Bali");
 		
