8b48db69f72eab61dd2ff9eb6f13629648ef3ff0
Maitane <maitaneartano@opendeusto.es>
2018-05-30 18:41:55 +0200

    avan

---
+++ EasyBookingServer/src/Services/UsuarioService.java
@@ -0,0 +1,29 @@
+package Services;
+
+import DB.DBManager;
+import Data.Usuario;
+
+public class UsuarioService
+
+{
+	private static UsuarioService instance;
+	private UsuarioService() {}
+	
+	public static UsuarioService getInstance()
+	{
+		if (instance == null)
+		{
+			instance = new UsuarioService();
+		}
+		
+		return instance;
+	}
+	
+	public boolean crearUsuario(String email, String password)
+	{
+		Usuario newUsuario = new Usuario(email, password);
+		return DBManager.getInstance().storeUsuario(newUsuario);
+	}
+	
+
+}
